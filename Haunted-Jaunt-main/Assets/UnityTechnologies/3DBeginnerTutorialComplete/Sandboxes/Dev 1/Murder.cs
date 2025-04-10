b7b41b0169443bbe90b78d8f168a1c19c7ab900a
Will Marceau <willfmarceau@gmail.com>
2025-04-10 15:00:02 -0700

    tagged ghosts and completed nearby enemy detection

--- Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 1/Murder.cs
+++ Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 1/Murder.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Takedown : MonoBehaviour
 {
+
+    public float takedownRange = 2f;
+    public LayerMask Enemy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,32 @@ public class Takedown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            Murder();
+        }
+    }
+
+    void Murder() 
+    {
+        // get enemies in takedown range
+        Collider[] closeEnemies = Physics.OverlapSphere(transform.position, takedownRange, Enemy);
+
+        // loop over close enemies
+        foreach (Collider enemy in closeEnemies) {
+
+            // log?
+            Debug.Log("Enemy in range: " + enemy.name);
         
+
+            // check if the enemy is ghost
+
+            // check if player is behind enemy
+
+            // check for line of sight
+
+            // destroy ghost
+
+            // break
+        }
     }
 }
