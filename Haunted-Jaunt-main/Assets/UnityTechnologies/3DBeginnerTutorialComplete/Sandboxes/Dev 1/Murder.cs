85b105f79befe7a25e4b7389ff4c82509b7cf598
Will Marceau <willfmarceau@gmail.com>
2025-04-10 15:31:44 -0700

    finished check for player being behind ghost for takedown

--- Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 1/Murder.cs
+++ Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 1/Murder.cs
@@ -27,11 +27,34 @@ public class Takedown : MonoBehaviour
         // loop over close enemies
         foreach (Collider enemy in closeEnemies) {
 
-            // log?
-            Debug.Log("Enemy in range: " + enemy.name);
-        
-
             // check if the enemy is ghost
+            if (enemy.CompareTag("Ghost")) 
+            {
+                // log?
+                Debug.Log("Enemy in range: " + enemy.name);
+
+                 // check if player is behind enemy
+
+                 // get the vector from the player to the enemy
+                 Vector3 toEnemy = (enemy.transform.position - transform.position).normalized;
+
+                 // get the vector of the players forward direction
+                 Vector3 playerDirection = transform.forward;
+
+                 // use dot product to compare the direction
+
+                 float dotProd = Vector3.Dot(playerDirection, toEnemy);
+
+                 // if in threshold
+                 if (dotProd > 0.8f) 
+                 {
+                    Debug.Log("Enemy can be Eliminated");
+                 }
+
+                 else {
+                    Debug.Log("Must be behind the enemy");
+                 }
+            }
 
             // check if player is behind enemy
 
