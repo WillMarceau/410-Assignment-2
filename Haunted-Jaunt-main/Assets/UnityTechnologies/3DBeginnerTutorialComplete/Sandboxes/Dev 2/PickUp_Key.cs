dec5673f3d7fd3b08e6d941aaa49f09dbe15a085
Kylie Griffiths <kcg3@icloud.com>
2025-04-11 11:38:23 -0700

    Working particles (knock on wood)

--- Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 2/PickUp_Key.cs
+++ Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 2/PickUp_Key.cs
@@ -1,19 +1,57 @@
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class PickUp_Key : MonoBehaviour
 {
-    public const float key_range = 0.5f;
-    private GameObject door_lock;
+    public const float key_range = 0.9f;
+    public GameObject door_lock;
+    private GameObject door;
     private GameObject key;
     private GameObject player;
+    public ParticleSystem ps;
+    private ParticleSystem.MainModule _main;
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         door_lock = GameObject.FindGameObjectWithTag("Lock");
         key = GameObject.FindGameObjectWithTag("Key");
         player = GameObject.FindGameObjectWithTag("Player");
+        door = GameObject.FindGameObjectWithTag("Open Door");
+        //ps = door_lock.GetComponent<ParticleSystem>();
+        _main = ps.main;
     }
 
+    void OnEnable()
+    {
+        //ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnParticleTrigger()
+    {
+        Gradient gradient = new Gradient();
+        GradientColorKey[] gradients = new GradientColorKey[2];
+        gradients[0] = new GradientColorKey(Color.red, 0.0f);
+        gradients[1] = new GradientColorKey(Color.blue, 1.0f);
+
+        GradientAlphaKey[] alphas = new GradientAlphaKey[2];
+        alphas[0] = new GradientAlphaKey(1.0f, 0.0f);
+        alphas[1] = new GradientAlphaKey(0.0f, 1.0f);
+
+        gradient.SetKeys(gradients,alphas);
+
+        _main.startColor = gradient;
+        _main.loop = false;
+        ps.Play();
+        AnimateDoor();
+    }
+
+    void AnimateDoor()
+    {
+        
+    }
     void OnTriggerEnter(Collider key) {
         if (key.gameObject.CompareTag("Key")) 
         {
@@ -29,6 +67,7 @@ public class PickUp_Key : MonoBehaviour
         }
     }
 
+
     void UnlockDoor() {
 
         float distanceToKey = Vector3.Distance(door_lock.transform.position, player.transform.position);
@@ -36,6 +75,9 @@ public class PickUp_Key : MonoBehaviour
             // Lock is close enough
             if (key.activeSelf == false) {
                 // User has key
+                OnParticleTrigger();
+                door_lock.SetActive(false);
+
             } else {
                 return;
             }
