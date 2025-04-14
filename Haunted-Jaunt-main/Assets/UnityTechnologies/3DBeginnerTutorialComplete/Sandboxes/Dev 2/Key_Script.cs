dc73337231349d68a123da7c1e10291352fa3c45
Kylie Griffiths <kcg3@icloud.com>
2025-04-14 10:13:24 -0700

    key pickup now makes noise

--- Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 2/Key_Script.cs
+++ Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 2/Key_Script.cs
@@ -5,6 +5,9 @@ public class Key_Script : MonoBehaviour
 {
 
     public bool following;
+    public int count; 
+    public AudioClip key_audio;
+    private AudioSource audioSource;
     private GameObject player;
     private PickUp_Key pickup;
 
@@ -13,6 +16,8 @@ public class Key_Script : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player");
         pickup = player.GetComponent<PickUp_Key>();
+        count = 0;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,6 +25,16 @@ public class Key_Script : MonoBehaviour
     {
         if (following == false) {
             transform.Rotate(0,100*Time.deltaTime, 0);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            if (count == 0) {
+                audioSource.PlayOneShot(key_audio);
+            }
+            
         }
     }
 }
