4b279d1e5577fee9002530d3f58046f27110dbca
Kylie Griffiths <kcg3@icloud.com>
2025-04-13 17:59:32 -0700

    key rotates on head

--- Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 2/Key_Script.cs
+++ Haunted-Jaunt-main/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Sandboxes/Dev 2/Key_Script.cs
@@ -1,16 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Key_Script : MonoBehaviour
 {
+
+    public bool following;
+    private GameObject player;
+    private PickUp_Key pickup;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        pickup = player.GetComponent<PickUp_Key>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Rotate(0,100*Time.deltaTime, 0);
+        if (following == false) {
+            transform.Rotate(0,100*Time.deltaTime, 0);
+        }
     }
 }
